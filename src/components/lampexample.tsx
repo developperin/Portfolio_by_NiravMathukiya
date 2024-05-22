394e3df7306e79a4edbd680b6b647efff38d7e9e
developer.in <xyz19072005@gmail.com>
2024-05-22 19:04:10 +0530

    Update lampexample.tsx

--- src/components/lampexample.tsx
+++ src/components/lampexample.tsx
@@ -19,14 +19,14 @@ export function LampDemo() {
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
          Join My Github  and Explore Now<br/>
-        <Link href={"https://github.com/developperin"} className="">
+        {/* <Link href={"https://github.com/developperin"} className="">
           <Button
             borderRadius="1.75rem"
             className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
           >
             Download Resume
           </Button>
-        </Link>
+        </Link> */}
       </motion.h1>
     </LampContainer>
   );
